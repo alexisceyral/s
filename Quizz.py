b3e3780f5c46ff93739ed586f720970894e74c7c
alexisceyral <alexisceyral@gmail.com>
2025-03-26 11:45:16 +0100

    Add files via upload

---
+++ Quizz.py
@@ -0,0 +1,57 @@
+
+print("Que permet d'afficher la commande suivante ?")
+print("print('Hello, World!')")
+print("A) Bonjour, Monde !")
+print("B) 'Hello, World!'")
+print("C) Hello, World!")
+a = str(input("Quel est la réponse (A, B ou C) ? "))
+if a == "C":
+    print("Bonne réponse")
+else:
+    print("Mauvaise réponse")
+
+print("Quel mot-clé est utilisé pour définir une fonction en Python ?")
+print("A) define")
+print("B) function")
+print("C) def")
+print("D) func")
+b = str(input("Quel est la réponse (A, B, C ou D) ? "))
+if b == "C":
+    print("Bonne réponse")
+else:
+    print("Mauvaise réponse")
+
+print("\nQuel est le type de la variable suivante ?")
+print("x = 3.14")
+print("A) int")
+print("B) float")
+print("C) double")
+print("D) str")
+c = str(input("Quel est la réponse (A, B, C ou D) ? "))
+if c == "B":
+    print("Bonne réponse")
+else:
+    print("Mauvaise réponse")
+
+print("\nQuelle est la sortie de ce code ?")
+print("print(2 * 'Python')")
+print("A) PythonPython")
+print("B) 2Python")
+print("C) Python2")
+print("D) Erreur")
+d = str(input("Quel est la réponse (A, B, C ou D) ? "))
+if d == "A":
+    print("Bonne réponse")
+else:
+    print("Mauvaise réponse")
+
+print("\nComment ajouter un élément à une liste en Python ?")
+print("A) list.append(element)")
+print("B) list.add(element)")
+print("C) list.insert(element)")
+print("D) list.push(element)")
+e = str(input("Quel est la réponse (A, B, C ou D) ? "))
+if e == "A":
+    print("Bonne réponse")
+else:
+    print("Mauvaise réponse")
