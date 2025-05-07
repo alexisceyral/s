c11f786e665676ecbbc7c1adb5d4fb843ef32ff3
alexisceyral <alexisceyral@gmail.com>
2025-05-07 11:23:53 +0200

    Update jeu_math.py

--- JeuInfo/jeu_math.py
+++ JeuInfo/jeu_math.py
@@ -6,25 +6,26 @@ def Jeu_traitre():
     window = tk.Tk()
     window.title("Jeu du Traître")
     window.geometry("600x400")
+    window.configure(background="#4e3629")
 
     traitre = random.randint(1, 200)  # Genere le premier traitre
 
-    # Fonction permettant de trouver le traitre
+    # Fonction permettant de trouver le traitre avec encapsulation ( toutes les fonctions ne fonctionneront sur dans Jeu_traitre)
     def trv_traitre():
         try:
             suspect = int(entry.get())  # Enregistre la valeur donnée par le joueur
             if suspect < traitre:
-                result_label.config(text=f"Plus grand que {suspect}")  # Indique si cest plus grand
+                result_label.config(text=f"Plus grand que {suspect}", fg="#ffd700")  # Indique si cest plus grand
             elif suspect > traitre:
-                result_label.config(text=f"Moins que {suspect}")  # Indique si cest plus petit
+                result_label.config(text=f"Moins que {suspect}", fg="#ff0000")  # Indique si cest plus petit
             elif suspect == traitre:
-                result_label.config(text="Félicitations! Vous avez trouvé le traitre!")
+                result_label.config(text="Félicitations! Vous avez trouvé le traitre!", fg="#d4af37")
                 messagebox.showinfo("Victoire!", "Vous avez trouvé le traitre!")
                 start_button.config(state="normal")
                 entry.config(state="disabled") # Désactive les bontons
                 guess_button.config(state="disabled")  # Désactive les bontons
         except ValueError:
-            result_label.config(text="Veuillez entrer un nombre valide.")  # Verifie si l'information entrée est bien un nombre
+            result_label.config(text="Veuillez entrer un nombre valide.", fg="#f1e1c6")  # Verifie si l'information entrée est bien un nombre
 
     # Fonction permettant de recommencer
     def restart_game():
@@ -34,9 +35,9 @@ def Jeu_traitre():
         guess_button.config(state="normal")
         result_label.config(text="")  # Efface la valeur du traitre
         start_button.config(state="disabled")
-
+    Police_med=("Garamond", 14,"bold")
     # Affiche les instructions
-    instruction_label = tk.Label(window, text="Devinez quel est le traitre (un nombre entre 1 et 200)!")
+    instruction_label = tk.Label(window, text="Devinez quel est le traitre (un nombre entre 1 et 200)!", font=Police_med, fg="#f42300", bg="#4e3629")
     instruction_label.pack(pady=10)
 
     entry = tk.Entry(window)
@@ -53,7 +54,7 @@ def Jeu_traitre():
     start_button.pack(pady=20)
 
 
-    window.mainloop() # Ouvre la fenetre
+    window.mainloop() # Ouvre la fenetree
 
 
 Jeu_traitre() # Lance le Jeu
